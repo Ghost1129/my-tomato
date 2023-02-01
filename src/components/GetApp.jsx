9791fd7b0d5ae014a09dc3f6711066e9a01147a7
Ghost <1129.gh0st@gmail.com>
2023-02-01 11:18:57 +0530

    Few Lines Added

--- src/components/GetApp.jsx
+++ src/components/GetApp.jsx
@@ -9,34 +9,41 @@ const GetApp = () => {
   return (
     <div className='w-full h-[30rem] flex items-center justify-center font-Popin bg-orange-50'>
         {/* Inner Body */}
-        <div className="w-[70%] h-full flex items-center justify-center">
+        <div className="w-[70%] md:w-[80%] h-full flex items-center justify-center">
             {/* Photo */}
             <div className='hidden md:flex items-center justify-center'><Image src={MobileImage} className="w-[250px] h-[280px]"/></div>
             {/* Text */}
-            <div>
-                <h3 className='text-3xl md:text-5xl md:font-medium'>Get the Tomato App</h3>
-                <span>We will send you a link, open it on your phone to download the app</span>
-                <div>
-                    <div>
-                        <input type="radio" name='radio' id='email' onClick={()=> setSelected(true)} defaultChecked={selected} />
+            <div className='text-center md:text-left'>
+                <h3 className='text-3xl md:text-5xl md:font-medium'>Get the Zomato App</h3>
+                <span className='text-sm md:text-lg font-light '>We will send you a link, open it on your phone to download the app</span>
+                <div className="flex gap-2 font-light">
+                    <div className='flex items-center gap-2 ml-4'>
+                        <input className='accent-red-500 w-4 h-4' type="radio" name='radio' id='email' onClick={()=> setSelected(true)} defaultChecked={selected} />
                         <label htmlFor='email'>Email</label>
                     </div>
-                    <div>
-                        <input type="radio" name='radio' id='phone' onClick={()=> setSelected(false)} />
+                    <div className='flex items-center gap-2'>
+                        <input className='accent-red-500 w-4 h-4' type="radio" name='radio' id='phone' onClick={()=> setSelected(false)} />
                         <label htmlFor='phone'>Phone</label>
                     </div>
                 </div>
                 <div className="flex">
+                    <div className='mr-4'>
+                    {selected ?
+                    <div className=''>
+                    <input type="text" placeholder='Email' className='border p-2 rounded-lg w-[90%]' />
+                    </div>:
                     <div>
-                        <input type="text" className='border' />
+                    <input type="text" placeholder='Phone' className='border' />
+                    </div> 
+                    }
                     </div>
-                    <button>Submit</button>
+                    <button className=' bg-red-400 text-white px-4 py-2 rounded-lg'>Submit</button>
                 </div>
-                <div>
-                    <span>Download App From</span>
-                    <div className='flex'>
-                        <Image src={PlayStore} className="rounded-lg"/>
-                        <Image src={AppStore} className="rounded-lg"/>
+                <div className='flex flex-col items-center md:items-start justify-center md:justify-start'>
+                    <span className='text-sm font-thin' >Download App From</span>
+                    <div className='flex gap-3 items-center justify-center '>
+                        <Image src={PlayStore} className="rounded-lg w-[120px] h-[40px]"/>
+                        <Image src={AppStore} className="rounded-lg w-[120px] h-[40px]"/>
                     </div>
                 </div>
             </div>
