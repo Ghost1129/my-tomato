af773b0103ab25e3fc853db1b02d7d30c439d769
Devansh Lodhi <1129.gh0st@gmail.com>
2023-07-08 13:09:15 +0530

    update

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -4,10 +4,14 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 import {AiOutlineMenu,AiOutlineShoppingCarte} from 'react-icons/ai'
 import {BsCart} from 'react-icons/bs'
+import { useDispatch, useSelector } from 'react-redux'
 import Searchbar from './Searchbar'
+import {selectedBasketItems} from '@/slices/basketSlice'
 
 const Navbar2 = () => {
+  const dispatch = useDispatch();
     const [menu, setMenu] = useState(false)
+    const basketItems = useSelector((state) => selectedBasketItems(state))
   return (
     <div className='flex items-center justify-between font-Popin w-full z-10 h-[70px] px-4 overflow-hidden'>
         <AiOutlineMenu className='cursor-pointer md:hidden' size={'1.6rem'} onClick={()=> setMenu(val => !val)}  />
@@ -19,7 +23,8 @@ const Navbar2 = () => {
             <Link href={{ pathname: '/cart'}} 
               className='relative mx-2'>
                 <BsCart className='cursor-pointer ' size={'1.6rem'}  />
-                <span className='absolute flex items-center justify-center w-5 h-5 bg-red-300 rounded-full -right-2 -top-2'>0
+                <span className='absolute flex items-center justify-center w-5 h-5 p-3 bg-red-300 rounded-full -right-2 -top-2'>
+                    {basketItems.length}
                 </span>
 
             </Link>
--- src/components/RestaurantHero.js
+++ src/components/RestaurantHero.js
@@ -5,19 +5,19 @@ import Camera from '@/assets/icons/photo-camera.png'
 
 const RestaurantHero = () => {
   return (
-    <div className='flex max-h-[420px] w-full'>
+    <div className='flex max-h-[520px] w-full overflow-hidden'>
         <div className='w-[100%] h-full md:max-h-[60%] flex flex-col md:flex-row mx-auto  '>
-            <div className='w-[100%] md:w-[60%] h-[100%] overflow-hidden'>
-                <Image src={food} className='object-cover w-full h-full transition-all duration-200 ease-in-out hover:scale-105'/>
+            <div className='w-[100%] md:w-[60%] h-[40%] overflow-hidden relative bg-black'>
+                <Image src={food} width={2000}   className='object-contain transition-all duration-200 ease-in-out hover:scale-105' alt='food'/>
             </div>  
-            <div className='w-[100%] md:w-[40%] gap-1 py-2 md:py-0 md:px-1  min-h-full grid grid-cols-2 overflow-hidden'>
-                <div className='overflow-hidden'><Image src={food} className='object-cover w-full h-full transition-all duration-200 ease-in-out hover:scale-105'/></div>
+            <div className='hidden w-[100%] md:w-[40%] gap-1 py-2 md:py-0 md:px-1  min-h-full md:grid grid-cols-2 overflow-hidden'>
+                <div className='overflow-hidden'><Image src={food} className='object-cover w-full h-full transition-all duration-200 ease-in-out hover:scale-105' alt='food'/></div>
                 <div className='relative overflow-hidden'>
                     <div className='absolute flex items-center justify-center w-full h-full cursor-pointer opacity-70 bg-red-50'> View More</div>
-                    <Image src={food} className='object-cover w-full h-full transition-all duration-200 ease-in-out hover:scale-105'/></div>
-                <div className='overflow-hidden'><Image src={food} className='object-cover w-full h-full transition-all duration-200 ease-in-out hover:scale-105'/></div>
+                    <Image src={food} className='object-cover w-full h-full transition-all duration-200 ease-in-out hover:scale-105' alt='food'/></div>
+                <div className='overflow-hidden'><Image src={food} className='object-cover w-full h-full transition-all duration-200 ease-in-out hover:scale-105' alt='food'/></div>
                 <div className='flex flex-col items-center justify-center overflow-hidden cursor-pointer bg-slate-100'>
-                    <Image src={Camera} className='w-5 h-5'/>
+                    <Image src={Camera} className='w-5 h-5' alt='camera'/>
                     <span>Add Photo</span>
 
                 </div>
